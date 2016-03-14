0400c7638d749a4806d09bbe044e295ecd57fa6c
Muntello <muntello@gmail.com>
2016-03-14 21:08:33 +0300

    Created Models
    Created models.py for q&a
    change path in .conf and init
    added database create to init

--- init.sh
+++ init.sh
@@ -1,5 +1,16 @@
+#nginx conf
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/ask
+
+#gunicorn conf
+sudo ln -s /home/box/etc/gunicorn.conf   /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart 
+
+#database conf
+mysql -uroot -e "CREATE DATABASE ASK"
+mysql -uroot -e "CREATE USER 'sa'@'localhost' IDENTIFIED BY 'sa'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON ASK.* TO 'sa'@'localhost'"
+
+
+python /home/box/ask/manage.py syncdb
